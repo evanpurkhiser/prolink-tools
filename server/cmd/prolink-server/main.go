9df96a2b7953547518619664f93aa9ece3a95532
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-09-21 03:18:04 -0700

    server: Add sentry

--- server/cmd/prolink-server/main.go
+++ server/cmd/prolink-server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"time"
 
+	"github.com/getsentry/raven-go"
 	"github.com/inconshreveable/log15"
 	"github.com/spf13/cobra"
 	"go.evanpurkhiser.com/prolink"
@@ -30,7 +31,7 @@ func startServer(cmd *cobra.Command, args []string) error {
 
 	log.Info("Starting prolink server", "version", Version)
 
-	prolink.Log = log.New("module", "prolink-lib")
+	prolink.Log = log.New("module", "prolink")
 
 	network, err := prolink.Connect()
 	if err != nil {
@@ -56,7 +57,20 @@ func startServer(cmd *cobra.Command, args []string) error {
 	return prolinkServer.Start()
 }
 
+func setupSentry() {
+	raven.SetDSN("https://5f4bb68c9b1d474ba2adc511885d9000:da119d611a3e41c19466dba5c1ce6000@sentry.io/279153")
+
+	if Version == "dev" {
+		raven.SetEnvironment("development")
+	} else {
+		raven.SetEnvironment("production")
+		raven.SetRelease(Version)
+	}
+}
+
 func main() {
+	setupSentry()
+
 	var rootCmd = &cobra.Command{
 		Use:   "prolink-server",
 		Short: "prolink-server provides a HTTP / websocket interface to a prolink network.",
@@ -67,5 +81,8 @@ func main() {
 	flags.IntP("port", "p", 8000, "port to start the HTTP server on")
 	flags.BoolP("debug", "d", false, "enables debug output")
 
-	rootCmd.Execute()
+	raven.CapturePanic(func() {
+		err := rootCmd.Execute()
+		raven.CaptureErrorAndWait(err, nil)
+	}, nil)
 }
