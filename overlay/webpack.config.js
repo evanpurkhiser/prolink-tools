b4946e84eee9de920acb4f13f007caeae80396fa
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-10-13 06:32:25 -0700

    overlay: Optimize for production
    Builds the overlay into a single html file

--- overlay/webpack.config.js
+++ overlay/webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 const IS_PROD =
   process.argv.find(a => a.includes('mode=production')) !== undefined;
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: 'index.html',
+    filename: 'prolink-overlay.html',
+    inlineSource: '.js$',
+  }),
+  new HtmlWebpackInlineSourcePlugin(),
+  IS_PROD ? null : new webpack.HotModuleReplacementPlugin(),
+];
+
+const babelPlugins = [
+  ['@babel/plugin-proposal-decorators', { legacy: true }],
+  ['@babel/plugin-transform-react-display-name'],
+  ['@babel/plugin-proposal-export-default-from'],
+  IS_PROD ? null : ['react-hot-loader/babel'],
+];
 
 module.exports = {
   entry: './src/index.js',
@@ -27,19 +45,14 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: [
-            ['@babel/plugin-proposal-decorators', { legacy: true }],
-            ['@babel/plugin-transform-react-display-name'],
-            ['@babel/plugin-proposal-export-default-from'],
-            ['react-hot-loader/babel'],
+          presets: [
+            ['@babel/preset-env', { targets: { chrome: '64' } }],
+            ['@babel/preset-react'],
           ],
+          plugins: babelPlugins.filter(x => x != null),
         },
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({ template: 'index.html' }),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
+  plugins: plugins.filter(x => x !== null),
 };
