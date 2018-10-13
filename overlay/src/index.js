f1d71a83814f2ec872af0c3dfbe9b0ea32f3d176
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-10-13 06:37:26 -0700

    overlay: Fix sentry

--- overlay/src/index.js
+++ overlay/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Sentry from '@sentry/browser';
+import { init } from '@sentry/browser';
 import { injectGlobal } from 'react-emotion';
 
 import App from 'app/components/app';
 
-Sentry.init({
-  dsn: 'https://f4f579e6703645e3bb3bc9cbd3f9b2fc@sentry.io/1300437',
-});
+init({ dsn: 'https://f4f579e6703645e3bb3bc9cbd3f9b2fc@sentry.io/1300437' });
 
 injectGlobal`
   * {
