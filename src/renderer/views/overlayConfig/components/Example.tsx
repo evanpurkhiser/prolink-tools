35007944c49f5da673dac27ed2c686b43bc23adc
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-11-19 05:25:54 -0800

    app: Adjust the example background for light text

--- src/renderer/views/overlayConfig/components/Example.tsx
+++ src/renderer/views/overlayConfig/components/Example.tsx
@@ -5,24 +5,12 @@ const Example = styled('div')`
   padding: 1.5rem;
   border: 1px solid #eee;
   border-radius: 0 0 3px 3px;
-  background-size: 2rem 2rem;
-  background-position: 0 0, 1rem 1rem;
-  background-image: linear-gradient(
-      45deg,
-      #f2f2f2 25%,
-      transparent 25%,
-      transparent 75%,
-      #f2f2f2 75%,
-      #f2f2f2
-    ),
-    linear-gradient(
-      45deg,
-      #f2f2f2 25%,
-      transparent 25%,
-      transparent 75%,
-      #f2f2f2 75%,
-      #f2f2f2
-    );
+  background-size: 20px 20px;
+  background-position: 0 0, 0 10px, 10px -10px, -10px 0px;
+  background-image: linear-gradient(45deg, #434343 25%, transparent 25%),
+    linear-gradient(-45deg, #434343 25%, transparent 25%),
+    linear-gradient(45deg, transparent 75%, #434343 75%),
+    linear-gradient(-45deg, #484848 75%, #434343 75%);
 `;
 
 export default Example;
