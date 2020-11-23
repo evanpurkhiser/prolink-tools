781aa20cd70e87d6c8a6ae9814262d192d425e16
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-11-23 00:59:12 -0800

    app: Move global styles setup to app.js

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -5,8 +5,10 @@ import * as Sentry from '@sentry/browser';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {when} from 'mobx';
+import {Global} from '@emotion/core';
 
 import Application from 'app/views/Application';
+import globalCss, {noSelect} from 'src/shared/globalCss';
 import {registerRendererIpc, registerRendererConfigIpc} from 'src/shared/store/ipc';
 import store from 'src/shared/store';
 
@@ -14,8 +16,14 @@ import store from 'src/shared/store';
 const mainElement = document.createElement('div');
 document.body.appendChild(mainElement);
 
-// Render components
-ReactDOM.render(<Application />, mainElement);
+const main = (
+  <React.Fragment>
+    <Global styles={[globalCss, noSelect]} />
+    <Application />
+  </React.Fragment>
+);
+
+ReactDOM.render(main, mainElement);
 
 registerRendererIpc();
 
--- src/renderer/views/Application.tsx
+++ src/renderer/views/Application.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
-import {Global} from '@emotion/core';
 import styled from '@emotion/styled';
 import {MemoryRouter, Switch, Route, Redirect} from 'react-router-dom';
 
-import globalCss, {noSelect} from 'src/shared/globalCss';
 import Titlebar from 'app/components/Titlebar';
 import Footer from 'app/components/Footer';
 import Devices from 'app/views/devices';
@@ -12,7 +10,6 @@ import Settings from 'app/views/settings';
 
 const Application = () => (
   <MemoryRouter>
-    <Global styles={[globalCss, noSelect]} />
     <Titlebar />
     <Frame>
       <Switch>
