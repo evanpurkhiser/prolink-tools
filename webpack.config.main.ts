3637c1ea3ff69969d137a6d669065c97072d664f
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-12-29 18:26:27 -0800

    app: Remove a bit of mikro-orm remnants

--- webpack.config.main.ts
+++ webpack.config.main.ts
@@ -14,26 +14,10 @@ const config: webpack.Configuration = merge(baseConfig, {
   },
   module: {
     rules: [
-      // We do not want ts-morph bundled up in the application, as it drags in
-      // typescript, which is huge. We are not using either of these at runtime, but
-      // they can't be ignored using IgnorePlugin because Mikro still requires them,
-      // and this causes an error at runtime. Packaging them with the null-loader
-      // allows them to be required without erroring then simply be swapped with null
-      // at runtime.
-      {
-        test: /(TsMorphMetadataProvider|ts-morph)/,
-        loader: 'null-loader',
-      },
       // Native modules
       {
         test: /\.node$/,
         use: 'node-loader',
-      },
-      // Some of MikroORM's dependencies use mjs files
-      {
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: 'javascript/auto',
       },
     ],
   },
