1c1891e35a1fdd68d8c9f7895add73319c7828da
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-01-10 05:42:29 -0800

    server: Count clients

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,22 +1,41 @@
+import Router from '@koa/router';
 import Koa from 'koa';
-import {Server} from 'socket.io';
+import {Server, Socket} from 'socket.io';
 
 import {createServer} from 'http';
 
+type Connection = {
+  socket: Socket;
+};
+
+const ingestMatcher = /^\/ingest\/([^/]+)$/;
+
+const connections: Record<string, Connection> = {};
+
 const app = new Koa();
 const server = createServer(app.callback());
-
 const wss = new Server(server);
 
-wss.of('/ingest').on('connection', socket => {
-  socket.on('store-init', console.log);
-  socket.on('store-update', console.log);
+const router = new Router();
+
+router.get('/stats', ctx => {
+  ctx.body = `Current number of clients: ${Object.keys(connections).length}`;
 });
 
-console.log('here we gooo');
+app.use(router.routes()).use(router.allowedMethods());
 
-app.use(ctx => {
-  ctx.body = 'Hello World';
+// Connect
+wss.of(ingestMatcher).on('connection', socket => {
+  const key = socket.nsp.name.match(ingestMatcher)[1];
+
+  connections[key] = {
+    socket,
+  };
+
+  socket.on('disconnect', () => delete connections[key]);
+
+  socket.on('store-init', console.log);
+  socket.on('store-update', console.log);
 });
 
 server.listen(8888);
