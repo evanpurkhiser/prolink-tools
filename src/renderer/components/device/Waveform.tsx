01a8f7c05fb956f622829febebe6e2265c88f951
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-03-04 19:39:49 -0800

    app: More waveform tinkering

--- src/renderer/components/device/Waveform.tsx
+++ src/renderer/components/device/Waveform.tsx
@@ -1,92 +1,99 @@
+import React from 'react';
+import styled from '@emotion/styled';
 import {autorun} from 'mobx';
 import {CDJStatus, DeviceID} from 'prolink-connect/lib/types';
 
 import {AppStore} from 'src/shared/store';
 import withStore from 'src/utils/withStore';
 
+function makeTrackLine(width: number, height: number) {
+  const offscreen = new OffscreenCanvas(width, height);
+  const osCtx = offscreen.getContext('2d')!;
+
+  // Draw 'track line'
+  osCtx.lineWidth = 2;
+  osCtx.strokeStyle = `#f2f2f2`;
+  osCtx.beginPath();
+  osCtx.moveTo(0, height / 2);
+  osCtx.lineTo(width, height / 2);
+  osCtx.stroke();
+
+  return offscreen;
+}
+
 function drawScrollingWaveform(
   store: AppStore,
   deviceId: DeviceID,
   canvas: HTMLCanvasElement
 ) {
-  const ctx = canvas.getContext('2d');
-
-  if (ctx === null) {
-    return;
-  }
-
   const width = canvas.parentElement?.clientWidth ?? 0;
   const height = canvas.parentElement?.clientHeight ?? 0;
+
+  const trackLine = makeTrackLine(width, height);
+
+  const zoom = 2;
+  const setgmentSize = 500;
+  const heightMultiplier = (height - 10) / 31;
+
+  let offscreen = new OffscreenCanvas(width, height);
+
+  // Whenever a new waveform is loaded, re-draw the offscreen waveform
+  const trackChangeDisposer = autorun(() => {
+    const waveformHd = store.devices.get(deviceId)?.waveforms?.waveformHd;
+    const grid = store.devices.get(deviceId)?.track?.beatGrid;
+
+    if (waveformHd === undefined) {
+      return;
+    }
+
+    const waveWidth = Math.round(waveformHd.length / zoom);
+    offscreen = new OffscreenCanvas(waveWidth, height);
+
+    const osCtx = offscreen.getContext('2d')!;
+
+    const segments = Math.ceil(waveformHd.length / setgmentSize);
+
+    const draw = async () => {
+      for (let i = 0; i < segments; i++) {
+        const start = i * setgmentSize;
+        const end = Math.min(start + setgmentSize, waveformHd.length);
+
+        for (let s = start; s < end; s++) {
+          const x = s / zoom;
+          const {height: segmentHeight, color} = waveformHd[s];
+          const [r, g, b] = color;
+
+          const size = segmentHeight * heightMultiplier;
+
+          const start = Math.round(height / 2 - size / 2);
+          const end = Math.round(height / 2 + size / 2);
+
+          osCtx.strokeStyle = `rgb(${r * 255}, ${g * 255}, ${b * 255})`;
+          osCtx.lineWidth = 1;
+          osCtx.beginPath();
+          osCtx.moveTo(0.5 + x, start);
+          osCtx.lineTo(0.5 + x, end);
+          osCtx.stroke();
+        }
+        await new Promise(resolve => setTimeout(resolve, 0));
+      }
+    };
+
+    draw();
+  });
 
   canvas.width = width;
   canvas.height = height;
 
-  const zoom = 1;
-  const heightMultiplier = height / 31;
+  const ctx = canvas.getContext('2d')!;
 
-  const offscreen = document.createElement('canvas');
+  const xOffset = width / 2;
 
-  autorun(() => {
-    const deviceStore = store.devices.get(deviceId);
-
-    if (deviceStore === undefined) {
-      return;
-    }
-
-    const {track} = deviceStore;
-
-    if (track === undefined) {
-      return;
-    }
-
-    // if (track.waveformHd === null) {
-    //   return;
-    // }
-
-    const waveformHd: any = [];
-
-    offscreen.width = Math.round(waveformHd.length / zoom);
-    offscreen.height = height;
-    const oscCtx = offscreen.getContext('2d');
-
-    if (oscCtx === null) {
-      return;
-    }
-
-    oscCtx.clearRect(0, 0, width, height);
-
-    // Draw 'track line'
-    oscCtx.lineWidth = 2;
-    oscCtx.strokeStyle = `#f2f2f2`;
-    oscCtx.beginPath();
-    oscCtx.moveTo(0, height / 2);
-    oscCtx.lineTo(width, height / 2);
-    oscCtx.stroke();
-
-    for (let s = 0; s < waveformHd.length; s++) {
-      const x = s / zoom;
-      const {height: segmentHeight, color} = waveformHd[s];
-      const [r, g, b] = color;
-
-      const size = segmentHeight * heightMultiplier;
-
-      const start = Math.round(height / 2 - size / 2);
-      const end = Math.round(height / 2 + size / 2);
-
-      oscCtx.strokeStyle = `rgb(${r * 255}, ${g * 255}, ${b * 255})`;
-      oscCtx.lineWidth = 1;
-      oscCtx.beginPath();
-      oscCtx.moveTo(0.5 + x, start);
-      oscCtx.lineTo(0.5 + x, end);
-      oscCtx.stroke();
-    }
-  });
-
-  let posX = 0;
+  let posX = 0 - xOffset;
   let pitch = 0;
   let playState: CDJStatus.PlayState = CDJStatus.PlayState.Paused;
 
-  autorun(() => {
+  const beatDisposer = autorun(() => {
     const deviceStore = store.devices.get(deviceId);
 
     if (deviceStore === undefined) {
@@ -105,30 +112,45 @@ function drawScrollingWaveform(
     console.log(pitch);
 
     const timeOffset = track.beatGrid?.[state?.beat ?? 0]?.offset ?? 0;
-    posX = Math.round(((timeOffset / 1000) * 150) / zoom);
+    posX = Math.round(((timeOffset / 1000) * 150) / zoom) - xOffset;
   });
 
   // const lastPosX = -1;
   let lastTs = 0;
 
+  let stop = false;
+
   const doDrawing = (ts: DOMHighResTimeStamp) => {
     ctx.clearRect(0, 0, width, height);
+    ctx.drawImage(trackLine, 0, 0, width, height);
     ctx.drawImage(offscreen, posX * -1, 0);
 
     if (lastTs === 0) {
       lastTs = ts;
     }
 
-    if (playState === CDJStatus.PlayState.Playing) {
+    if (
+      playState === CDJStatus.PlayState.Playing ||
+      playState === CDJStatus.PlayState.Cuing
+    ) {
       // milliseconds since last draw, convert to seconds, 150 pixels per second.
       posX += ((ts - lastTs) / 1000) * 150;
     }
 
     lastTs = ts;
-    window.requestAnimationFrame(doDrawing);
+
+    if (!stop) {
+      window.requestAnimationFrame(doDrawing);
+    }
   };
 
   window.requestAnimationFrame(doDrawing);
+
+  return () => {
+    beatDisposer();
+    trackChangeDisposer();
+    stop = true;
+  };
 }
 
 type Props = {
@@ -136,24 +158,21 @@ type Props = {
   deviceId: DeviceID;
 };
 
-const startedFor: {[k: number]: boolean} = {};
-
 const Waveform = ({store, deviceId}: Props) => {
-  const handleCanvasRef = (canvas: HTMLCanvasElement | null) => {
-    if (canvas === null) {
-      return;
-    }
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
-    if (startedFor[deviceId]) {
-      return;
-    }
+  React.useEffect(() => drawScrollingWaveform(store, deviceId, canvasRef.current!), []);
 
-    startedFor[deviceId] = true;
-
-    drawScrollingWaveform(store, deviceId, canvas);
-  };
-
-  return <canvas ref={handleCanvasRef} />;
+  return (
+    <Container>
+      <canvas ref={canvasRef} />
+    </Container>
+  );
 };
+
+const Container = styled('div')`
+  height: 60px;
+  width: 100%;
+`;
 
 export default withStore(Waveform);
